adaf97dc7d0734a3e284c499f68b210b8ea7fdbe
vyacheslavv44 <follow.trend7777@gmail.com>
2020-03-15 01:32:36 +0400

    feat: add required functionality to work of form

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 const NAVIGATION = document.getElementById('navigation');
-/*const BUTTON = document.*/
+const BUTTON = document.getElementById('sendButton');
+const CLOSE_BUTTON = document.getElementById('closeButton');
+const subjectFieldVariable = document.getElementById('subjectField');
+const textareaFieldVariable = document.getElementById('textareaField');
 
 
 
 NAVIGATION.addEventListener('click', (event) => {
     NAVIGATION.querySelectorAll('a').forEach(el => el.classList.remove('focused'));
     event.target.classList.add('focused');
+});
+
+BUTTON.addEventListener('click', () => {
+    const subject = document.getElementById('subject').value.toString();
+    const personComment = document.getElementById('personComment').value.toString();
+
+    /*document.getElementById('subjectField').innerText = subject;*/
+    if (subject.length == 0) {
+        subjectFieldVariable.innerText = 'Без темы';
+    } else {
+        subjectFieldVariable.innerText = subject;
+        document.getElementById('temaP').classList.remove('collaps__p__tema');
+    }
+    /*document.getElementById('textareaField').innerText = personComment;*/
+    if (personComment.length == 0) {
+        textareaFieldVariable.innerText = 'Без описания ';
+    } else {
+        textareaFieldVariable.innerText = personComment;
+        document.getElementById('opisanieP').classList.remove('collaps__p__opisanie');
+    }
+    document.getElementById('confirmationMessageBlock').classList.remove('hidden');
+});
+
+CLOSE_BUTTON.addEventListener('click', () => {
+    document.getElementById('subjectField').innerText = '';
+    document.getElementById('textareaField').innerText = '';
+    document.getElementById('nearFooterForm').reset();
+    document.getElementById('confirmationMessageBlock').classList.add('hidden');
+    document.getElementById('temaP').classList.add('collaps__p__tema');
+    document.getElementById('opisanieP').classList.add('collaps__p__opisanie');
 });
